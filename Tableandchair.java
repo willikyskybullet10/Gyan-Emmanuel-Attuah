0475b4ea3dee02a1fe670e9f55856f8c4a1ec705
Gyan Emmanuel Attuah <willikyskybullet@gmail.com>
2026-06-11 20:39:23 +0000

    Add files via upload

---
+++ Tableandchair.java
@@ -0,0 +1,21 @@
+public class Table {
+    public static void main (String[] args){
+
+        System.out.println("x                                       x");
+        System.out.println("x                                       x");
+        System.out.println("x                                       x");
+        System.out.println("x                                       x");
+        System.out.println("x             xxxxxxxxxxxx              x");
+        System.out.println("xxxxxxxxxxx   x          x     xxxxxxxxxx");
+        System.out.println("x         x   x          x     x        x");
+        System.out.println("x         x   x          x     x        x");
+        System.out.println("x         x   x          x     x        x");
+        System.out.println("x         x   x          x     x        x");
+
+
+
+    }
+
+
+
+}
---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+    public static void main (String[] args) {
+
+        System.out.println("                TT                 ");
+        System.out.println("              TTTTTT               ");
+        System.out.println("            TTTTTTTTTT             ");
+        System.out.println("          TTTTTTTTTTTTTT           ");
+        System.out.println("        TTTTTTTTTTTTTTTTTT         ");
+        System.out.println("      TTTTTTTTTTTTTTTTTTTTTT       ");
+        System.out.println("    TTTTTTTTTTTTTTTTTTTTTTTTTT     ");
+        System.out.println("  TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT   ");
+        System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT ");
+
+    }
+}
